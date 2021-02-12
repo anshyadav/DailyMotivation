7477f1363d4fd44cbcb19c3ff3c504e43aec8583
anshyadav <yadavansh04@gmail.com>
2021-02-12 20:26:27 +0000

    Final Changes

---
+++ site.js
@@ -0,0 +1,13 @@
+let quoteElement = document.getElementById("quote");
+let imageElement = document.getElementById("quote-image");
+
+
+
+function refreshData(){
+    fetch("https://api.kanye.rest").then(response => response.json()).then(quotation => {
+        quoteElement.innerHTML = quotation.quote
+    });
+    fetch("https://api.thecatapi.com/v1/images/search?size=full").then(response => response.json()).then(cats => {
+        imageElement.innerHTML = "<img src=" + cats[0].url + " class='img' alt = 'My Cat'>"
+    }); 
+}
