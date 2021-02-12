f8803fc03f43dd8859394243c886eda4be93569e
anshyadav <yadavansh04@gmail.com>
2021-02-12 21:14:40 +0000

    Updated the JS Again

--- site.js
+++ site.js
@@ -4,7 +4,7 @@ let imageElement = document.getElementById("quote-image");
 
 
 function refreshData(){
-    fetch("https://api.thecatapi.com/v1/images/search?size=full").then(response => response.json()).then(cats => {
+    fetch("https://api.thecatapi.com/v1/images/search").then(response => response.json()).then(cats => {
         imageElement.innerHTML = "<img src=" + cats[0].url + " class='img' alt = 'My Cat'>"
     }); 
     fetch("https://api.kanye.rest").then(response => response.json()).then(quotation => {
